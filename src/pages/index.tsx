e4f6e68eaf248582e9050fbf9f81fb616d359934
Sehan MacPro <ianparkfirst@gmail.com>
2022-05-22 20:25:31 +0900

    feat: mint fx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,51 +33,50 @@ const Index: NextPage = () => {
 					relative
 					hScreen
 				>
-						<Div absolute top24 left24 w90 imgTag src={IMAGES.logos.webeWhiteLogo}></Div>
-						<Div onClick={() => setClicked((prev) => !prev)}>
-							{clicked ? <Div absolute z999 top24 right24 w18 imgTag src={IMAGES.cancelIcon}/> : <Div absolute z999 top24 right24 w18 imgTag src={IMAGES.menuIcon}/>}
-							{clicked && (
-								<Div
-								absolute
-								top0
-								wFull
-								hFull
-								style={{background: "rgba(237, 238, 233, 0.96)"}}
-								>
-									<Div mt90 ml35 textPrimary fontSize32>
-										<Div flex mb38 itemsCenter aTag href={"https://soonilabs.notion.site/GOMZ-9708c13f69c94ed39672ac8c1b7b8e12"}>
-											<Div w30 imgTag src={IMAGES.logos.webePrimaryIcon}></Div>
-											<Div ml18>Public Docs</Div>
-										</Div>
-										<Div flex mb38 itemsCenter aTag href={"https://betterworldapp.io"}>
-											<Div w32 imgTag src={IMAGES.logos.betterWorldPrimaryLogo}></Div>
-											<Div ml17>BetterWorld</Div>
-										</Div>
-										<Div flex mb38 itemsCenter aTag href={"https://discord.com/invite/7tV3WxWf8p"}>
-											<FaDiscord size={32} />
-											<Div ml18>Discord</Div>
-										</Div>
-										<Div flex mb38 itemsCenter aTag href={"https://twitter.com/officialgomz"}>
-											<FaTwitter size={32} />
-											<Div ml18>Twitter</Div>
-										</Div>
-										<Div flex mb38 itemsCenter aTag href={"https://instagram.com/offical_gomz"}>
-											<FaInstagram size={32} />
-											<Div ml18>Instagram</Div>
-										</Div>
+					<Div absolute top24 left24 w90 imgTag src={IMAGES.logos.webeWhiteLogo}></Div>
+					<Div onClick={() => setClicked((prev) => !prev)}>
+						{clicked ? (
+							<Div absolute z999 top24 right24 w18 imgTag src={IMAGES.cancelIcon} />
+						) : (
+							<Div absolute z999 top24 right24 w18 imgTag src={IMAGES.menuIcon} />
+						)}
+						{clicked && (
+							<Div absolute top0 wFull hFull style={{ background: "rgba(237, 238, 233, 0.96)" }}>
+								<Div mt90 ml35 textPrimary fontSize32>
+									<Div flex mb38 itemsCenter aTag href={"https://soonilabs.notion.site/GOMZ-9708c13f69c94ed39672ac8c1b7b8e12"}>
+										<Div w30 imgTag src={IMAGES.logos.webePrimaryIcon}></Div>
+										<Div ml18>Public Docs</Div>
+									</Div>
+									<Div flex mb38 itemsCenter aTag href={"https://betterworldapp.io"}>
+										<Div w32 imgTag src={IMAGES.logos.betterWorldPrimaryLogo}></Div>
+										<Div ml17>BetterWorld</Div>
+									</Div>
+									<Div flex mb38 itemsCenter aTag href={"https://discord.com/invite/7tV3WxWf8p"}>
+										<FaDiscord size={32} />
+										<Div ml18>Discord</Div>
+									</Div>
+									<Div flex mb38 itemsCenter aTag href={"https://twitter.com/officialgomz"}>
+										<FaTwitter size={32} />
+										<Div ml18>Twitter</Div>
+									</Div>
+									<Div flex mb38 itemsCenter aTag href={"https://instagram.com/offical_gomz"}>
+										<FaInstagram size={32} />
+										<Div ml18>Instagram</Div>
 									</Div>
 								</Div>
-							)}
-						</Div>
+							</Div>
+						)}
+					</Div>
 					<Div flex justifyCenter>
 						<Div mt104 w346 imgTag src={IMAGES.gomzMainAstronaut}></Div>
 					</Div>
 					<Div flex justifyCenter>
-						<Div flex justifyCenter itemsCenter mt45 bgSecondary roundedFull w150 h50 textSecondary2 fontSize20 onClick={handleClickHome}>ENTER</Div>
+						<Div flex justifyCenter itemsCenter mt45 bgSecondary roundedFull w150 h50 textSecondary2 fontSize20 onClick={handleClickHome}>
+							ENTER
+						</Div>
 					</Div>
 				</Div>
 			</>
-					
 		);
 	}
 	return (
@@ -86,7 +85,7 @@ const Index: NextPage = () => {
 			<Div
 				style={{
 					background: "linear-gradient(114.31deg, #3E4071 51.1%, #37315A 89.59%)",
-					webkitTextStroke: "1px #000"
+					webkitTextStroke: "1px #000",
 				}}
 				hScreen
 				relative
@@ -94,49 +93,85 @@ const Index: NextPage = () => {
 				{/* <Div absolute w250 imgTag src={IMAGES.starDusts}></Div> */}
 				<Div absolute w400 bottom0 right0 imgTag src={IMAGES.starDusts3}></Div>
 				<Div absolute top0 wFull z200 flex px80 mt40>
-						<Div flex1></Div>
-						<Div flex flexRow itemsCenter>
-							<Div
-								mx10
-								textWhite
-								clx={"hover:opacity-50"}
-								cursorPointer
-								aTag
-								href={"https://soonilabs.notion.site/GOMZ-9708c13f69c94ed39672ac8c1b7b8e12"}
-							>
-								<Div roundedFull bgSecondary py4 px18 fontSize12 trackingWidest borderBlack border1
-								style={{webkitTextStroke: "1px #000"}}>
-									Public Docs
-								</Div>
+					<Div flex1></Div>
+					<Div flex flexRow itemsCenter>
+						<Div
+							mx10
+							textWhite
+							clx={"hover:opacity-50"}
+							cursorPointer
+							aTag
+							href={"https://soonilabs.notion.site/GOMZ-9708c13f69c94ed39672ac8c1b7b8e12"}
+						>
+							<Div roundedFull bgSecondary py4 px18 fontSize12 trackingWidest borderBlack border1 style={{ webkitTextStroke: "1px #000" }}>
+								Public Docs
 							</Div>
-							<Div mx10 clx={"hover:opacity-50"} cursorPointer aTag href={"https://betterworldapp.io"}>
-								<Div imgTag src={IMAGES.logos.betterWorld} h20 wAuto></Div>
-							</Div>
-							<Div mx10 textSecondary2 clx={"hover:opacity-50"} cursorPointer aTag href={"https://discord.com/invite/7tV3WxWf8p"}>
-								<FaDiscord size={30} />
-							</Div>
-							<Div mx10 textSecondary2 clx={"hover:opacity-50"} cursorPointer aTag href={"https://twitter.com/officialgomz"}>
-								<FaTwitter size={25} />
-							</Div>
-							<Div mx10 textSm textSecondary={nextLocale==LOCALES.KO} textSecondary2={nextLocale!=LOCALES.KO} clx={"hover:opacity-50"} cursorPointer onClick={() => reloadWithLocale(LOCALES.KO)}>KOR</Div>
-							<Div textSm textSecondary2>l</Div>
-							<Div ml10 textSm textSecondary={nextLocale==LOCALES.EN} textSecondary2={nextLocale!=LOCALES.EN} clx={"hover:opacity-50"} cursorPointer onClick={() => reloadWithLocale(LOCALES.EN)}>ENG</Div>
 						</Div>
+						<Div mx10 clx={"hover:opacity-50"} cursorPointer aTag href={"https://betterworldapp.io"}>
+							<Div imgTag src={IMAGES.logos.betterWorld} h20 wAuto></Div>
+						</Div>
+						<Div mx10 textSecondary2 clx={"hover:opacity-50"} cursorPointer aTag href={"https://discord.com/invite/7tV3WxWf8p"}>
+							<FaDiscord size={30} />
+						</Div>
+						<Div mx10 textSecondary2 clx={"hover:opacity-50"} cursorPointer aTag href={"https://twitter.com/officialgomz"}>
+							<FaTwitter size={25} />
+						</Div>
+						<Div
+							mx10
+							textSm
+							textSecondary={nextLocale == LOCALES.KO}
+							textSecondary2={nextLocale != LOCALES.KO}
+							clx={"hover:opacity-50"}
+							cursorPointer
+							onClick={() => reloadWithLocale(LOCALES.KO)}
+						>
+							KOR
+						</Div>
+						<Div textSm textSecondary2>
+							l
+						</Div>
+						<Div
+							ml10
+							textSm
+							textSecondary={nextLocale == LOCALES.EN}
+							textSecondary2={nextLocale != LOCALES.EN}
+							clx={"hover:opacity-50"}
+							cursorPointer
+							onClick={() => reloadWithLocale(LOCALES.EN)}
+						>
+							ENG
+						</Div>
+					</Div>
 				</Div>
 				<Div flex hFull relative>
 					<Div flexCol z100 ml200 mt100>
 						<Div w300 imgTag src={IMAGES.logos.webeWhiteLogo}></Div>
 						<Div mt35 textLeft fontSize25 textSecondary2 leadingTight>
-							Cultural franchise dedicated to global innovators,<br></br>party heads, and Web 3 enthusiasts</Div>
-						<Div mt35 clx={"hover:opacity-50"} w140 bgSecondary roundedFull px40 py8 fontSize25 textWhite borderBlack border1
-						onClick={handleClickMint} cursorPointer>
-							MINT</Div>
-						<Div mt45 w250 imgTag src={IMAGES. mintingProcess}></Div>
+							Cultural franchise dedicated to global innovators,<br></br>party heads, and Web 3 enthusiasts
+						</Div>
+						<Div
+							mt35
+							clx={"hover:opacity-50"}
+							w140
+							bgSecondary
+							roundedFull
+							px40
+							py8
+							fontSize25
+							textWhite
+							borderBlack
+							border1
+							onClick={handleClickMint}
+							cursorPointer
+						>
+							MINT
+						</Div>
+						<Div mt45 w250 imgTag src={IMAGES.mintingProcess}></Div>
 					</Div>
 					<Div absolute w350 top110 right250 z999 imgTag src={IMAGES.gomzMainAstronaut} cursorPointer onClick={handleClickHome}></Div>
-						<Div absolute top200 right100 textSecondary2 textXl>
-							Click Me To Enter!
-						</Div>
+					<Div absolute top200 right100 textSecondary2 textXl>
+						Click Me To Enter!
+					</Div>
 				</Div>
 			</Div>
 		</>
--- src/pages/mint/index.tsx
+++ src/pages/mint/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
 import { useState } from "react";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import Col from "src/components/Col";
 import Div from "src/components/Div";
+import Row from "src/components/Row";
 import { useSmartContract } from "src/hooks/contract/kip17";
 import { useCaver } from "src/hooks/wallet/kaikas";
+import { COLORS } from "src/modules/constants";
 import { IMAGES } from "src/modules/images";
 import { href } from "src/modules/routeHelper";
 import { urls } from "src/modules/urls";
@@ -12,6 +16,7 @@ const Index: NextPage = () => {
 	const handleClickGomz = () => {
 		href(urls.index);
 	};
+	const price = 150;
 	const [error, setError] = useState("");
 	const [amountToMint, setAmountToMint] = useState(1);
 	const smartContract = useSmartContract();
@@ -28,7 +33,7 @@ const Index: NextPage = () => {
 					const mintPrepareResponse = await smartContract.methods.mint(amountToMint).send({
 						from: selectedAddress,
 						gas: "7500000",
-						value: caver.utils.toPeb(150, "KLAY"),
+						value: caver.utils.toPeb(price, "KLAY"),
 					});
 				}
 			} catch (error) {
@@ -38,58 +43,111 @@ const Index: NextPage = () => {
 			console.log("Please install Kaikas");
 		}
 	};
+	const handlePressUp = () => {
+		if (amountToMint > 4) return;
+		setAmountToMint((prevAmount) => prevAmount + 1);
+	};
+	const handlePressDown = () => {
+		if (amountToMint < 2) return;
+		setAmountToMint((prevAmount) => prevAmount - 1);
+	};
 	return (
 		<Div
 			style={{
 				background: "#EDEEE9",
 			}}
 			relative
-			hScreen>
-            <Div wFull h110 bgPrimary flex itemsCenter z2 borderB1 borderBlack style={{webkitTextStroke: "1px #000",}}>
-                <Div ml250 w200 hAuto imgTag src={IMAGES.logos.webeWhiteLogo}></Div>
-                <Div ml20 textWhite text3xl>Air</Div>
-                <Div absolute right150 textWhite fontSize32>OnBoarding Pass</Div>
-            </Div>
-            <Div wFull h10 z1 bgSecondary borderB1 borderBlack></Div>
-            <Div flex overMono fontSize25>
-                <Div my250 ml90 w50 h50 imgTag src={IMAGES.circleLeftArrow} onClick={handleClickGomz} cursorPointer></Div>
-                <Div ml100 my120>
-                    <Div flex>
-                        <Div mr20>WALLET ADDRESS:</Div>
-                        <Div w220 h40 borderBlack border1 roundedFull balooB textTransparent fontSize25 style={{webkitTextStroke: "1px #000"}} flex justifyCenter>
-                        Connect Wallet</Div>
-                    </Div>
-                    <Div flex mt40>
-                        <Div mr20>FLIGHT:</Div>
-                        <Div fontBold mr20>WhereverYouAre</Div>
-                        <Div mr20>TO</Div>
-                        <Div fontBold>BetterWorld</Div>
-                    </Div>
-                    <Div flex mt40>
-                        <Div mr20>SEAT TYPE:</Div>
-                        <Div fontBold>CohortA</Div>
-                    </Div>
-                    <Div mt40>AVAILABLE SEATS:</Div>
-                </Div>
-                <Div absolute top475 right160 borderDashed border1 w700 h0 rotate90 borderBlack></Div>
-                <Div absolute top205 right110>
-                    <Div flex mt40>
-                        <Div mr20>PRICE:</Div>
-                        <Div fontBold>150 KLAY</Div>
-                    </Div>
-                    <Div flex mt40>
-                        <Div mr20>AMOUNT:</Div>
-                        <Div fontBold> 1 per Wallet</Div>
-                    </Div>
-                    <Div mt40 w230 h70 borderBlack border1 roundedFull textWhite bgPrimary fontSize36 balooB
-                    style={{webkitTextStroke: "1px #000"}} flex justifyCenter itemsCenter>
-                        MINT
-                    </Div>
-                </Div>
-            </Div>
-            <Div absolute bottom0 right0 w600 z999 imgTag src={IMAGES.webeAir}></Div>
-        </Div>
-    )
-}
+			hScreen
+		>
+			<Div wFull h110 bgPrimary flex itemsCenter z2 borderB1 borderBlack style={{ webkitTextStroke: "1px #000" }}>
+				<Div ml250 w200 hAuto imgTag src={IMAGES.logos.webeWhiteLogo}></Div>
+				<Div ml20 textWhite text3xl>
+					Air
+				</Div>
+				<Div absolute right150 textWhite fontSize32>
+					OnBoarding Pass
+				</Div>
+			</Div>
+			<Div wFull h10 z1 bgSecondary borderB1 borderBlack></Div>
+			<Div flex overMono fontSize25>
+				<Div my250 ml90 w50 h50 imgTag src={IMAGES.circleLeftArrow} onClick={handleClickGomz} cursorPointer></Div>
+				<Div ml100 my120>
+					<Div flex>
+						<Div mr20>WALLET ADDRESS:</Div>
+						<Div
+							w220
+							h40
+							borderBlack
+							border1
+							roundedFull
+							balooB
+							textTransparent
+							fontSize25
+							style={{ webkitTextStroke: "1px #000" }}
+							flex
+							justifyCenter
+						>
+							Connect Wallet
+						</Div>
+					</Div>
+					<Div flex mt40>
+						<Div mr20>FLIGHT:</Div>
+						<Div fontBold mr20>
+							WhereverYouAre
+						</Div>
+						<Div mr20>TO</Div>
+						<Div fontBold>BetterWorld</Div>
+					</Div>
+					<Div flex mt40>
+						<Div mr20>SEAT TYPE:</Div>
+						<Div fontBold>CohortA</Div>
+					</Div>
+					<Div mt40>AVAILABLE SEATS:</Div>
+				</Div>
+				<Div absolute top475 right160 borderDashed border1 w700 h0 rotate90 borderBlack></Div>
+				<Div absolute top205 right110>
+					<Div flex mt40>
+						<Div mr20>PRICE:</Div>
+						<Div fontBold>{price * amountToMint} KLAY</Div>
+					</Div>
+					<Div flex mt40>
+						<Div mr20>AMOUNT:</Div>
+						<Row>
+							<Col auto onClick={handlePressDown} cursorPointer>
+								<FaChevronDown color={amountToMint > 1 ? "black" : COLORS.GRAY400} />
+							</Col>
+							<Col auto fontBold>
+								{amountToMint}
+							</Col>
+							<Col auto onClick={handlePressUp} cursorPointer>
+								<FaChevronUp color={amountToMint < 5 ? "black" : COLORS.GRAY400} />
+							</Col>
+						</Row>
+					</Div>
+					<Div
+						mt40
+						w230
+						h70
+						borderBlack
+						border1
+						roundedFull
+						textWhite
+						bgPrimary
+						fontSize36
+						balooB
+						style={{ webkitTextStroke: "1px #000" }}
+						flex
+						justifyCenter
+						itemsCenter
+						onPress={mint}
+					>
+						MINT
+					</Div>
+				</Div>
+			</Div>
+			<Div absolute bottom0 right0 w600 z999 imgTag src={IMAGES.webeAir}></Div>
+		</Div>
+	);
+};
 
 export default Index;
