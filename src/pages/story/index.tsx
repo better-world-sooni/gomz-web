5702b0f40f9cd85e718b42c97cda1200ed6e9768
Sehan MacPro <ianparkfirst@gmail.com>
2022-06-28 23:52:00 +0900

    fix: story

--- src/pages/story/index.tsx
+++ src/pages/story/index.tsx
@@ -6,107 +6,13 @@ import { Tween, Timeline } from "react-gsap";
 import Footer from "src/components/common/Footer";
 import EmptyBlock from "src/components/EmptyBlock";
 import MainTopBar from "src/components/common/MainTopBar";
-import { useState } from "react";
 import useIsTablet from "src/hooks/useIsTablet";
 import { wording } from "src/wording/wording";
 import { useRouter } from "next/router";
-import { Faq } from "src/components/Faq";
 
 const Index: NextPage = () => {
 	const isTablet = useIsTablet();
-	const [clicked, setClicked] = useState(false);
 	const { locale } = useRouter();
-	const team = [
-		{
-			name: wording.team.index.members.jieun.name[locale],
-			imageUri: IMAGES.team.jieun,
-			position: wording.team.index.members.jieun.position[locale],
-			desc: wording.team.index.members.jieun.desc[locale],
-			specialty: wording.team.index.members.jieun.specialty[locale],
-			countries: wording.team.index.members.jieun.countries,
-		},
-		{
-			name: wording.team.index.members.jade.name[locale],
-			imageUri: IMAGES.team.seonghyun,
-			position: wording.team.index.members.jade.position[locale],
-			desc: wording.team.index.members.jade.desc[locale],
-			specialty: wording.team.index.members.jade.specialty[locale],
-			countries: wording.team.index.members.jade.countries,
-		},
-		{
-			name: wording.team.index.members.minjun.name[locale],
-			imageUri: IMAGES.team.mj,
-			position: wording.team.index.members.minjun.position[locale],
-			desc: wording.team.index.members.minjun.desc[locale],
-			specialty: wording.team.index.members.minjun.specialty[locale],
-			countries: wording.team.index.members.minjun.countries,
-		},
-		{
-			name: wording.team.index.members.yeajean.name[locale],
-			imageUri: IMAGES.team.serena,
-			position: wording.team.index.members.yeajean.position[locale],
-			desc: wording.team.index.members.yeajean.desc[locale],
-			specialty: wording.team.index.members.yeajean.specialty[locale],
-			countries: wording.team.index.members.yeajean.countries,
-		},
-		{
-			name: wording.team.index.members.eric.name[locale],
-			imageUri: IMAGES.team.eric,
-			position: wording.team.index.members.eric.position[locale],
-			desc: wording.team.index.members.eric.desc[locale],
-			specialty: wording.team.index.members.eric.specialty[locale],
-			countries: wording.team.index.members.eric.countries,
-		},
-		{
-			name: wording.team.index.members.ian.name[locale],
-			imageUri: IMAGES.team.ian,
-			position: wording.team.index.members.ian.position[locale],
-			desc: wording.team.index.members.ian.desc[locale],
-			specialty: wording.team.index.members.ian.specialty[locale],
-			countries: wording.team.index.members.ian.countries,
-		},
-		{
-			name: wording.team.index.members.jaehwan.name[locale],
-			imageUri: IMAGES.team.jay,
-			position: wording.team.index.members.jaehwan.position[locale],
-			desc: wording.team.index.members.jaehwan.desc[locale],
-			specialty: wording.team.index.members.jaehwan.specialty[locale],
-			countries: wording.team.index.members.jaehwan.countries,
-		},
-		{
-			name: wording.team.index.members.seungan.name[locale],
-			imageUri: IMAGES.team.seungan,
-			position: wording.team.index.members.seungan.position[locale],
-			desc: wording.team.index.members.seungan.desc[locale],
-			specialty: wording.team.index.members.seungan.specialty[locale],
-			countries: wording.team.index.members.seungan.countries,
-		},
-	];
-	const faqs = [
-		{
-			question: "Why 8,888?",
-			answer:
-				"Eight signifies various aspects of life. Wealth, prosperity, ∞ (infinity), and many more. Out of all the different meanings, WeBe has chosen 8,888 due to ∞ . Despite all the external factors in the NFT space, such as volatile floor prices, rug pulls, and ponzi-like schemes, WeBe ensures an innovative community that will be infinite. We are here to create a culture of its own with 8,888 WeBe. ",
-		},
-		{
-			question: "Why Klaytn?",
-			answer:
-				"WeBe decided to launch on Klaytn for the following reasons— energy efficient, low latency, easy access to Kakao Talk, and stable gas fee.",
-		},
-		{
-			question: "How to mint?",
-			answer:
-				"WeBe's minting process is divided into 4 categories (Cohort A, Cohort B, Cohort C, and Public Minting). To be selected for Cohort A, B, or C, you must either 1) be invited by a cohort member, 2) submit a WeBe whitelist application, or 3) be selected for a whitelisted event. From whitelist to public minting, all sales are performed on this website. For secondary sales, it will be available via OpenSea.",
-		},
-		{
-			question: "Mint date & price?",
-			answer: "Whitelist Minting: Date- TBD I Price- 100 KLAY Public Minting: Date- TBD I Price: 200 KLAY",
-		},
-		{
-			question: "How many WeBe can I mint?",
-			answer: "To ensure a wide range of community, we are limiting holders to five WeBe per wallet",
-		},
-	];
 	if (isTablet) {
 		return (
 			<>
@@ -294,6 +200,7 @@ const Index: NextPage = () => {
 										</Div>{" "}
 										manner possible.
 									</Div>
+									<EmptyBlock h={100} />
 								</Div>
 							</Scene>
 						</Controller>
