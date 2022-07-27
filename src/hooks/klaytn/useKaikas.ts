19410c1f3aaa5cbe37105dc723aedd7a679e6373
Sehan MacPro <ianparkfirst@gmail.com>
2022-07-27 20:51:32 +0900

    fix: revert

--- src/hooks/klaytn/useKaikas.ts
+++ src/hooks/klaytn/useKaikas.ts
@@ -13,23 +13,27 @@ export const useKaikas = () => {
     const alertChangeChain = () => dispatch(alertModalAction({ enabled: true }));
 
     useEffect(() => {
-        if(klaytn && klaytn._kaikas.isEnabled()){
-            if(!publicRuntimeConfig.CONF_IS_DEVELOPMENT){
-                if(klaytn.networkVersion == '8217'){
-                    setKaikas(klaytn)
+        // if(klaytn){
+        //     if(!publicRuntimeConfig.CONF_IS_DEVELOPMENT){
+        //         if(klaytn.networkVersion == '8217'){
+        //             setKaikas(klaytn)
+        //             klaytn?.on('accountsChanged', function(accounts) {
+        //                 setKaikas({...klaytn, selectedAddress: accounts[0]})
+        //             })
+        //         } else {
+        //             alertChangeChain()
+        //         }
+        //     } else {
+        //         setKaikas(klaytn)
+        //         klaytn?.on('accountsChanged', function(accounts) {
+        //             setKaikas({...klaytn, selectedAddress: accounts[0]})
+        //         })
+        //     }
+        // }
+        setKaikas(klaytn)
                     klaytn?.on('accountsChanged', function(accounts) {
                         setKaikas({...klaytn, selectedAddress: accounts[0]})
                     })
-                } else {
-                    alertChangeChain()
-                }
-            } else {
-                setKaikas(klaytn)
-                klaytn?.on('accountsChanged', function(accounts) {
-                    setKaikas({...klaytn, selectedAddress: accounts[0]})
-                })
-            }
-        }
     }, [typeof window, klaytn, klaytn?._kaikas.isEnabled(), klaytn?.selectedAddress])
     return kaikas
 }
