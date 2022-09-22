5722cd7d6f07c9ed72e74747720c83e160902f6a
jiyong-j <98527707+jiyong-j@users.noreply.github.com>
2022-09-23 00:18:04 +0900

    feat: Scrolling event

--- src/components/common/drafts.tsx
+++ src/components/common/drafts.tsx
@@ -1,8 +1,8 @@
 import Div from "src/components/Div";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { Iphone, iphoneSize } from "./iphone";
 import { IMAGES } from "src/modules/images";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const time=0.5
 const hoverscale=1.1
@@ -18,11 +18,22 @@ const showTime = 2
 	);
 }
 
+const UseScrollProgress = () => {
+	const ref = useRef(null);
+	const { scrollYProgress } = useScroll({
+	  target: ref,
+	  offset: ["70% end", "start 20%"]
+	});
+	return ({ref, scrollYProgress})
+}
+
 const SplashDraft = ({iPhoneWidth, isTablet, showTime}) => {
 	const [hover1, sethover1] = useState(false);
 	const [hover1Mobile, setHover1Mobile] = useState(false);
 	const { show:hover1MobileTimer } = UseTimerState(hover1Mobile, showTime, () => setHover1Mobile(false));
-	
+	const {ref, scrollYProgress} = UseScrollProgress()
+	const scaleBack = useTransform(scrollYProgress, [0,1], [1,10])
+	const scaleFront = useTransform(scrollYProgress, [0,1], [1,1.5])
 	const hover = isTablet ? hover1MobileTimer : hover1
 	const draft1 = {
 		hover:{
@@ -64,12 +75,12 @@ const SplashDraft = ({iPhoneWidth, isTablet, showTime}) => {
 	}
 	return (
 		<Draft draft={draft1} onMouseEnter={isTablet ? ()=>setHover1Mobile(true):()=>sethover1(true)} onMouseLeave={!isTablet&&(()=>sethover1(false))} iPhoneWidth={iPhoneWidth} content={
-			<Div absolute wFull hFull bgWhite flex flexCol itemsCenter justifyCenter>
+			<Div ref={ref} absolute wFull hFull bgWhite flex flexCol itemsCenter justifyCenter>
 				<Div relative w80 h80 top={40} left={40}>
-					<Div absolute _translateY1over2 _translateX1over2><motion.div animate={draft1.hover.display1.animate} transition={draft1.hover.display1.transition} style={{width:"100%", height:"100%"}}><Div imgTag src={IMAGES.appDrafts.draft1.bwLogo1}/></motion.div></Div>
-					<Div absolute _translateY1over2 _translateX1over2><motion.div animate={draft1.hover.display2.animate} transition={draft1.hover.display2.transition} style={{width:"100%", height:"100%"}}><Div imgTag src={IMAGES.appDrafts.draft1.bwLogo2}/></motion.div></Div>
+					<Div absolute _translateY1over2 _translateX1over2><motion.div style={{width:"100%", height:"100%", scale:scaleBack}}><Div imgTag src={IMAGES.appDrafts.draft1.bwLogo1}/></motion.div></Div>
+					<Div absolute _translateY1over2 _translateX1over2><motion.div style={{width:"100%", height:"100%", scale:scaleFront}}><Div imgTag src={IMAGES.appDrafts.draft1.bwLogo2}/></motion.div></Div>
 				</Div>
-				<motion.div animate={draft1.hover.display3.animate} transition={draft1.hover.display3.transition} style={{position:"relative"}}><Div maxH={10} maxH15={!isTablet} imgTag src={IMAGES.appDrafts.draft1.bwWord}/></motion.div>
+				<motion.div style={{position:"relative", opacity:scrollYProgress}}><Div maxH={10} maxH15={!isTablet} imgTag src={IMAGES.appDrafts.draft1.bwWord}/></motion.div>
 			</Div>
 		}/>
 	)
@@ -81,7 +92,9 @@ const SocialDraft = ({iPhoneWidth, isTablet, showTime}) => {
 	const [changeDraft, setChangeDraft] = useState(true);
 	const onClickToChangeContent=()=>setChangeDraft(!changeDraft)
 	const { show:hover2MobileTimer } = UseTimerState(hover2Mobile, showTime, () => setHover2Mobile(false));
-	
+	const {ref, scrollYProgress} = UseScrollProgress()
+	const scrollProgress = useTransform(scrollYProgress, value=>`${(value)*100}%`)
+	const opacity = useTransform(scrollYProgress, [0.1, 0.4], [0, 1])
 	const hover = isTablet ? hover2MobileTimer : hover2
 	const draft2 = {
 		hover:{
@@ -133,16 +146,16 @@ const SocialDraft = ({iPhoneWidth, isTablet, showTime}) => {
 	}
 	return (
 		<Draft draft={draft2} onMouseEnter={isTablet ? ()=>setHover2Mobile(true):()=>sethover2(true)} onMouseLeave={!isTablet&&(()=>sethover2(false))} onClick={onClickToChangeContent} iPhoneWidth={iPhoneWidth} content={
-			<Div absolute wFull hFull bgWhite>
+			<Div ref={ref} absolute wFull hFull bgWhite>
 				<>
 					<motion.div animate={draft2.show(changeDraft, true).animate} transition={draft2.show(changeDraft, 0).transition} style={{position:'absolute', width:"100%", height:"100%"}}>
-						<ContentImage animate={draft2.hover.feed.animate} transition={draft2.hover.feed.transition} src={IMAGES.appDrafts.draft2.feed.content}/>
+						<ContentImage scrollProgress={scrollProgress} animate={draft2.hover.feed.animate} transition={draft2.hover.feed.transition} src={IMAGES.appDrafts.draft2.feed.content}/>
 						<ContentImage src={IMAGES.appDrafts.draft2.feed.head}/>
 					</motion.div>
 					<motion.div animate={draft2.show(changeDraft, false).animate} transition={draft2.show(changeDraft, 0).transition} style={{position:'absolute', width:"100%", height:"100%"}}>
-						<ContentImage animate={draft2.hover.profile.content.animate} transition={draft2.hover.profile.content.transition} src={IMAGES.appDrafts.draft2.profile.content}/>
+						<ContentImage scrollProgress={scrollProgress} animate={draft2.hover.profile.content.animate} transition={draft2.hover.profile.content.transition} src={IMAGES.appDrafts.draft2.profile.content}/>
 						<ContentImage src={IMAGES.appDrafts.draft2.profile.head1}/>
-						<motion.div animate={draft2.hover.profile.head.animate} transition={draft2.hover.profile.head.transition} style={{position:'absolute', width:"100%", height:"100%"}}>
+						<motion.div style={{position:'absolute', width:"100%", height:"100%", opacity:opacity}}>
 							<Div wFull imgTag src={IMAGES.appDrafts.draft2.profile.head2}></Div>
 						</motion.div>
 					</motion.div>
@@ -152,16 +165,24 @@ const SocialDraft = ({iPhoneWidth, isTablet, showTime}) => {
 	)
 }
 
-const ContentImage = ({animate=null, transition=null, src}) => {
+const ContentImage = ({animate=null, transition=null, scrollProgress=null, src}) => {
 	return (
-		animate&&transition ?<motion.div animate={animate} transition={transition} style={{
+		scrollProgress ? <motion.div style={{
 			position:"absolute",
             height:"100%",
             width:"100%",
             backgroundImage: `url(${src})`,
             backgroundSize: "cover",
             backgroundPositionX: "center",
-        }}/> : <Div absolute wFull imgTag src={src}/>
+			backgroundPositionY: scrollProgress,
+        }}/>: (animate&&transition ?<motion.div animate={animate} transition={transition} style={{
+			position:"absolute",
+            height:"100%",
+            width:"100%",
+            backgroundImage: `url(${src})`,
+            backgroundSize: "cover",
+            backgroundPositionX: "center",
+        }}/> : <Div absolute wFull imgTag src={src}/>)
 	)
 }
 
@@ -187,9 +208,10 @@ const UseTimerState=(condtion, seconds, closecontidion = null)=>{
 	return { show };
 }
 
-export function Draft({draft=null, onMouseEnter=null, onMouseLeave=null, onClick=null, content=null, iPhoneWidth}) {
+export function Draft({draft=null, onMouseEnter=null, onMouseLeave=null, onClick=null, content=null, iPhoneWidth, ref=null}) {
 	return (
-		<Div onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick} cursorPointer>
+		<Div>
+		<Div ref={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick} cursorPointer>
 			{draft?.showtransition ? <motion.li variants={draft.showtransition} style={{width:"100%", height:"100%"}}>
 			{draft?.hover?.draft ? <motion.div animate={draft.hover.draft.animate} transition={draft.hover.draft.transtion}>
 			<Iphone relative w={iPhoneWidth}>
@@ -214,6 +236,7 @@ export function Draft({draft=null, onMouseEnter=null, onMouseLeave=null, onClick
 			</Iphone>
 			)
 			}
+		</Div>
 		</Div>
 	)
 }
--- src/components/common/readstory.tsx
+++ src/components/common/readstory.tsx
@@ -1,10 +1,22 @@
 import Div from "src/components/Div";
 import { IMAGES } from "src/modules/images";
-import { motion } from "framer-motion";
-import { useState } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef, useState } from "react";
+
+const UseScrollProgress = (isTablet) => {
+	const ref = useRef(null);
+	const { scrollYProgress } = useScroll({
+	  target: ref,
+	  offset: ["start 90%", "start 30%"]
+	});
+    const scrollProgress = useTransform(scrollYProgress, value=>`${isTablet? 50+(value)*50:(value)*100}%`)
+    const opacity = useTransform(scrollYProgress, value=> value*0.6)
+	return ({ref, scrollProgress, opacity})
+}
 
 export default function ReadStroy({isTablet, handleClickReadStory}) {
     const [hover, setHover] = useState(false)
+    const {ref, scrollProgress, opacity} = UseScrollProgress(isTablet)
     return (
         <Div
         data-aos="fade-up" 
@@ -19,17 +31,19 @@ export default function ReadStroy({isTablet, handleClickReadStory}) {
         cursorPointer
         overflowHidden
         >
-        <Div absolute hFull wFull><motion.div animate={{ backgroundPositionX: hover?"100%":"50%"}} transition={{duration:0.5}} style={{
+        <Div absolute hFull wFull><motion.div style={{
             height:"100%",
             width:"100%",
             backgroundImage: `url(${isTablet ? IMAGES.storyBannerMobile: IMAGES.storyBanner})`,
             backgroundSize: "cover",
             backgroundPositionY: "center",
+            backgroundPositionX : scrollProgress,
         }}/></Div>
-        <Div absolute hFull wFull><motion.div animate={{ opacity: hover?0.6:0}} transition={{duration:0.5}} style={{
+        <Div ref={ref} absolute hFull wFull><motion.div style={{
             height:"100%",
             width:"100%",
             backgroundColor: "black",
+            opacity: opacity,
         }}/></Div>
         <Div relative px50 px30={isTablet} flex flexCol justifyCenter>
             <Div textWhite fontSize18 fontSize14={isTablet}>
