dc17c0b835e9a868ba7b6dcf37b7354999ab8c8e
jiyong-j <98527707+jiyong-j@users.noreply.github.com>
2022-09-28 21:36:37 +0900

    feat: Introduce Webe animated

--- src/components/common/IntroduceWebe.tsx
+++ src/components/common/IntroduceWebe.tsx
@@ -1,15 +1,49 @@
 import Div from "../Div";
 import { IMAGES } from "src/modules/images";
-import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
-import { useRef, useState } from "react";
+import { motion, useSpring, useTime, useTransform } from "framer-motion";
+import { useState } from "react";
+import EmptyBlock from "../EmptyBlock";
 
-const number = 9
+const animateDuration = 1.2
+const animateDelay = -0.2
+const changeScale = 1.1
+const numberDesktop = 9
+const numberMobile = 8
 
-const IntroduceWebe = ({wording, locale}) => {
+export const IntroduceWebeMobile = ({wording, locale}) => {
+    return (
+        <>
+        <Div textWhite fontSize36 leadingNone data-aos="fade-up" px30>
+						We Be
+						<Div spanTag textSecondary>
+							{" "}
+							Weird, Different, and Free.
+						</Div>
+					</Div>
+					<EmptyBlock h={30} />
+					<Div fontSize15 balooR px30 mt5 data-aos="fade-up">
+						{wording.story.index.value[locale]}
+					</Div>
+					<EmptyBlock h={30} />
+					<Div grid gridCols4 gapX={"3vw"} gapY={"3vw"} px30 data-aos="fade-up">
+                        <PfpImages isTablet={true}/>
+						{/* {new Array(12).fill(null).map((_, index) => {
+							return (
+								<Div key={index} inlineBlock>
+									<Div imgTag src={IMAGES.examplePfp[index + 1]} roundedLg shadowLg></Div>
+								</Div>
+							);
+						})} */}
+        </Div>
+        </>
+    )
+}
+
+export const IntroduceWebe = ({wording, locale}) => {
     return (
         <Div flex gapX={50} itemsCenter data-aos="fade-up">
         <Div grid gridCols3 gapX={20} gapY={20}>
-            <PfpImages/>
+            <PfpImages isTablet={false}/>
         </Div>
 
         <Div flex1>
@@ -28,26 +62,18 @@ const IntroduceWebe = ({wording, locale}) => {
     )
 }
 
-const PfpImages = () => {
-    const [imageIndex, setImageIndex] = useState([8, 6, 3, 4, 7, 5, 0, 1, 2]);
-    const currentKey = useRef(number-1)
+const PfpImages = ({isTablet}) => {
+    const number = isTablet ? numberMobile:numberDesktop
+    const time = useTime()
+    const imageIndex = isTablet ? [6, 3, 2, 7, 5, 0, 1, 4] : [8, 6, 3, 2, 7, 5, 0, 1, 4];
     const imageKeyLength = Object.keys(IMAGES.examplePfp).length
-    const getNextImageKey = () => {
-        currentKey.current = (currentKey.current+1)%(imageKeyLength)
-        return currentKey.current
-    }
-    const setImageKey = () => {
-        var array = [...imageIndex]
-        const nextKey = getNextImageKey()
-        array[imageIndex.indexOf(((nextKey-number+imageKeyLength)%imageKeyLength))] = nextKey
-        setImageIndex(array)
-    }
+    const images = (key)=>IMAGES.examplePfp[key+1]
     return (
         <>{
             imageIndex.map((value, index) => {
             return (
                 <Div key={index} inlineBlock>
-                    <PfpImage value={value} onClick={()=>setImageKey()} image={IMAGES.examplePfp[value+1]}/>
+                    <PfpImage imageKey={value} images={images} imageKeyLength={imageKeyLength} standardTime={time} isTablet={isTablet} number={number}/>
                 </Div>
             );
         })
@@ -55,10 +81,43 @@ const PfpImages = () => {
     )
 }
 
-const PfpImage = ({value, onClick, image}) => {
+const PfpImage = ({imageKey, images, imageKeyLength, standardTime, isTablet, number}) => {
+    const [hover, setHover] = useState(false)
+    const duration = animateDuration*1000
+    const delay = animateDelay*1000
+    const totalTime = (duration+delay)*number
+    const time = useTransform(standardTime, [0,100],[0+(duration+delay)*(imageKeyLength-imageKey),100+(duration+delay)*(imageKeyLength-imageKey)],{clamp:false})
+    const scale = useTransform(time, value=> hover ? (changeScale+0.1):(value%totalTime<duration ? 
+        changeScale
+        : 1))
+    const basicOpacity = useTransform(time, value=>value%totalTime<duration ? 
+    1-1/duration*(value%totalTime)
+    : (
+        value%totalTime<totalTime/2 ?
+        0
+        :
+    1))
+    const basicImageUri = useTransform(time, value => 
+        value%totalTime < (totalTime-animateDuration)/4+animateDuration ? 
+        `url(${images((imageKey+(((value-(value%totalTime))/totalTime)-1)*number+imageKeyLength)%imageKeyLength)})`
+        :
+        `url(${images((imageKey+((value-(value%totalTime))/totalTime)*number)%imageKeyLength)})`
+        )
+    const toImageUri = useTransform(time, value => 
+        value%totalTime < totalTime-(totalTime-animateDuration)/4 ? 
+        `url(${images((imageKey+(((value-(value%totalTime))/totalTime))*number)%imageKeyLength)})`
+        :
+        `url(${images((imageKey+(((value-(value%totalTime))/totalTime)+1)*number)%imageKeyLength)})`
+        )
+    const toOpacity = useTransform(basicOpacity, value => 1-value)
+    const scaleSpringMotionValue = useSpring(scale, {stiffness:100, damping:20})
+
     return (
-        <motion.div onClick={onClick} initial={{scale:1.1}} animate={{scale:1}} exit={{scale:1.1}}><Div imgTag src={image} roundedLg shadowLg w100 h100/></motion.div>
+        <motion.div onHoverStart={()=>{setHover(true)}} onHoverEnd={()=>{setHover(false)}} style={{width:isTablet ? "19vw":100, height:isTablet ? "19vw":100, position:"relative", scale:scaleSpringMotionValue}}>
+            <Div roundedLg shadowLg style={{width:"100%", height:"100%",position:"relative"}} overflowHidden>
+                <motion.div style={{width:"100%", height:"100%", position:"absolute", backgroundImage: toImageUri, backgroundSize: "cover", backgroundPositionX: "center", backgroundPositionY: "center", opacity:toOpacity}}/>
+                <motion.div style={{width:"100%", height:"100%", position:"absolute", backgroundImage: basicImageUri, backgroundSize: "cover", backgroundPositionX: "center", backgroundPositionY: "center", opacity:basicOpacity}}/>
+            </Div>
+        </motion.div>
     )
 }
-
-export default IntroduceWebe
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ import UseTimeButton from "src/components/common/useTimeButton";
 import GotoBetterWorldButton from "src/components/common/BetterWorldButton";
 import { motion, LayoutGroup } from "framer-motion";
 import Teams from "src/components/common/team";
-import IntroduceWebe from "src/components/common/IntroduceWebe";
+import { IntroduceWebeMobile, IntroduceWebe} from "src/components/common/IntroduceWebe";
 
 const Index: NextPage = () => {
 	const isTablet = useIsTablet();
@@ -121,27 +121,7 @@ const Index: NextPage = () => {
 				<Div style={{ background: "linear-gradient(180deg, #37315A 0%, #413F70 72%, rgb(102, 129, 197) 100%)" }} relative>
 					<motion.div layout transition={{duration:0.3}}>
 					<EmptyBlock h={100} />
-					<Div textWhite fontSize36 leadingNone data-aos="fade-up" px30>
-						We Be
-						<Div spanTag textSecondary>
-							{" "}
-							Weird, Different, and Free.
-						</Div>
-					</Div>
-					<EmptyBlock h={30} />
-					<Div fontSize15 balooR px30 mt5 data-aos="fade-up">
-						{wording.story.index.value[locale]}
-					</Div>
-					<EmptyBlock h={30} />
-					<Div grid gridCols4 gapX={20} gapY={15} px30 data-aos="fade-up">
-						{new Array(12).fill(null).map((_, index) => {
-							return (
-								<Div key={index} inlineBlock>
-									<Div imgTag src={IMAGES.examplePfp[index + 1]} roundedLg shadowLg></Div>
-								</Div>
-							);
-						})}
-					</Div>
+					<IntroduceWebeMobile wording={wording} locale={locale}/>
 					<EmptyBlock h={100} />
 					<Div px15>
 						<ReadStroy isTablet={isTablet} handleClickReadStory={handleClickReadStory}/>
