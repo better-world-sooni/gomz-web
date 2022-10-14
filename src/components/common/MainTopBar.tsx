d901086048442764a9deb1a929422d7a591edb33
jiyong-j <98527707+jiyong-j@users.noreply.github.com>
2022-10-14 16:37:02 +0900

    fix: MainTopBar Webe Logo

--- src/components/common/MainTopBar.tsx
+++ src/components/common/MainTopBar.tsx
@@ -19,117 +19,91 @@ import { BetterWorldModal } from "../modal/BetterWorldModal";
 import { AlertModal } from "../modal/AlertModal";
 
 function MainTopBar({ absolute = false }) {
-	const isTablet = useIsTablet();
-	const [clicked, setClicked] = useState(false);
-	const { locale } = useRouter();
-	const dispatch = useDispatch();
-	const handleClickWebe = () => {
-		href(urls.index);
-	};
-	const handleClickCheckOnMyWebes = () => {
-		href(urls["my-webes"].index);
-	};
-	const handleClickBetterWorld = () => {
-		dispatch(betterWorldModalAction({ enabled: true }));
-	};
-	const kaikas = useKaikas();
-	const { balance } = useAddressState({
-		kaikas,
-	});
-	const connectWallet = async () => {
-		if (kaikas) {
-			await kaikas.enable();
-		}
-	};
+  const isTablet = useIsTablet();
+  const [clicked, setClicked] = useState(false);
+  const { locale } = useRouter();
+  const dispatch = useDispatch();
+  const handleClickWebe = () => {
+    href(urls.index);
+  };
+  const handleClickCheckOnMyWebes = () => {
+    href(urls["my-webes"].index);
+  };
+  const handleClickBetterWorld = () => {
+    dispatch(betterWorldModalAction({ enabled: true }));
+  };
+  const kaikas = useKaikas();
+  const { balance } = useAddressState({
+    kaikas,
+  });
+  const connectWallet = async () => {
+    if (kaikas) {
+      await kaikas.enable();
+    }
+  };
 
-	if (isTablet) {
-		return (
-			<>
-				<Div wFull z30 px15 py15 bgSecondary absolute>
-					<Div flex mxAuto maxW={1150} itemsCenter>
-						<Div onClick={handleClickWebe} cursorPointer>
-							<Div w70 imgTag src={IMAGES.logos.webeLogo} onClick={handleClickWebe} cursorPointer></Div>
-						</Div>
-						<Div flex1></Div>
-						<Div onClick={() => setClicked((prev) => !prev)}>
-							{clicked ? <FaTimes size={25} color={COLORS.SECONDARY2} /> : <FaBars size={25} color={COLORS.SECONDARY2} />}
-						</Div>
-					</Div>
-				</Div>
-				{clicked && (
-					<Div absolute z20 top0 wFull hScreen style={{ background: COLORS.SECONDARY, backdropFilter: "blur(20px)" }}>
-						<Div mt100 mx15 textSecondary2 fontSize24>
-						<Div
-								mt20
-								flex
-								px30
-								py10
-								border2
-								borderSecondary2
-								bgSecondary
-								roundedLg
-								itemsCenter
-								cursorPointer
-								aTag
-								href={"https://discord.com/invite/7tV3WxWf8p"}
-							>
-								<Div mr18>Discord</Div>
-								<FaDiscord size={20} />
-							</Div>
-							<Div
-								mt20
-								flex
-								px30
-								py10
-								border2
-								borderSecondary2
-								bgSecondary
-								roundedLg
-								itemsCenter
-								cursorPointer
-								aTag
-								href={"https://www.betterworldapp.io"}
-							>
-								<Div mr18>BetterWorld</Div>
-								<Div w20 imgTag src={IMAGES.logos.betterWorld}></Div>
-							</Div>
-							<Div
-								mt20
-								flex
-								px30
-								py10
-								border2
-								borderSecondary2
-								bgSecondary
-								roundedLg
-								itemsCenter
-								cursorPointer
-								aTag
-								href={"https://twitter.com/webe_weirdbears"}
-							>
-								<Div mr18> Twitter</Div>
-								<FaTwitter size={20} />
-							</Div>
-							<Div
-								mt20
-								flex
-								px30
-								py10
-								border2
-								borderSecondary2s
-								bgSecondary
-								roundedLg
-								itemsCenter
-								cursorPointer
-								aTag
-								href={"https://instagram.com/weirdbears"}
-							>
-								<Div mr18>Instagram</Div>
-								<FaInstagram size={20} />
-							</Div>
-						</Div>
-						<Div flex justifyCenter mt20 mx15>
-							{/* <Div
+  if (isTablet) {
+    return (
+      <>
+        <Div wFull z30 px15 py15 bgSecondary absolute>
+          <Div flex mxAuto maxW={1150} itemsCenter>
+            <Div onClick={handleClickWebe} cursorPointer>
+              <Div w70 imgTag src={IMAGES.logos.webeLogoProduction} onClick={handleClickWebe} cursorPointer></Div>
+            </Div>
+            <Div flex1></Div>
+            <Div onClick={() => setClicked((prev) => !prev)}>
+              {clicked ? <FaTimes size={25} color={COLORS.SECONDARY2} /> : <FaBars size={25} color={COLORS.SECONDARY2} />}
+            </Div>
+          </Div>
+        </Div>
+        {clicked && (
+          <Div absolute z20 top0 wFull hScreen style={{ background: COLORS.SECONDARY, backdropFilter: "blur(20px)" }}>
+            <Div mt100 mx15 textSecondary2 fontSize24>
+              <Div
+                mt20
+                flex
+                px30
+                py10
+                border2
+                borderSecondary2
+                bgSecondary
+                roundedLg
+                itemsCenter
+                cursorPointer
+                aTag
+                href={"https://discord.com/invite/7tV3WxWf8p"}
+              >
+                <Div mr18>Discord</Div>
+                <FaDiscord size={20} />
+              </Div>
+              <Div mt20 flex px30 py10 border2 borderSecondary2 bgSecondary roundedLg itemsCenter cursorPointer aTag href={"https://www.betterworldapp.io"}>
+                <Div mr18>BetterWorld</Div>
+                <Div w20 imgTag src={IMAGES.logos.betterWorld}></Div>
+              </Div>
+              <Div
+                mt20
+                flex
+                px30
+                py10
+                border2
+                borderSecondary2
+                bgSecondary
+                roundedLg
+                itemsCenter
+                cursorPointer
+                aTag
+                href={"https://twitter.com/webe_weirdbears"}
+              >
+                <Div mr18> Twitter</Div>
+                <FaTwitter size={20} />
+              </Div>
+              <Div mt20 flex px30 py10 border2 borderSecondary2s bgSecondary roundedLg itemsCenter cursorPointer aTag href={"https://instagram.com/weirdbears"}>
+                <Div mr18>Instagram</Div>
+                <FaInstagram size={20} />
+              </Div>
+            </Div>
+            <Div flex justifyCenter mt20 mx15>
+              {/* <Div
 								flex1
 								flex
 								justifyCenter
@@ -156,53 +130,46 @@ function MainTopBar({ absolute = false }) {
 							>
 								ENGLISH
 							</Div> */}
-						</Div>
-					</Div>
-				)}
-				<BetterWorldModal />
-			</>
-		);
-	}
-	return (
-		<Div sticky={!absolute} absolute={absolute} top0 wFull z200 px80 py12 bgSecondary={!absolute} bgBlack={absolute} bgOpacity30={absolute}>
-			<MintingModal />
-			<InviteModal />
-			<RebirthModal />
-			<BetterWorldModal />
-			<AlertModal />
-			<Div flex mxAuto maxW={1150}>
-				<Div onClick={handleClickWebe} cursorPointer>
-					<Div w100 imgTag src={IMAGES.logos.webeLogo}></Div>
-				</Div>
-				<Div flex1></Div>
-				<Div flex flexRow itemsCenter>
-					{balance > 0 && (
-						<Div mr10 textSecondary2 cursorPointer onClick={handleClickCheckOnMyWebes}>
-							<Div roundedFull py4 pr10 fontSize12 trackingWidest clx={"group transition hover:text-primary-light"}>
-								MY WEBES
-							</Div>
-						</Div>
-					)}
-					<Div
-						mr10
-						textSecondary2
-						clx={"group transition hover:text-primary-light"}
-						cursorPointer
-						aTag
-						href={"https://discord.com/invite/7tV3WxWf8p"}
-					>
-						<FaDiscord size={30} />
-					</Div>
-					<Div mx10 clx={"group transition hover:opacity-50"} cursorPointer aTag href={"https://www.betterworldapp.io"}>
-						<Div imgTag src={IMAGES.logos.betterWorld} h25 wAuto></Div>
-					</Div>
-					<Div mx10 textSecondary2 clx={"group transition hover:text-primary-light"} cursorPointer aTag href={"https://twitter.com/webe_weirdbears"}>
-						<FaTwitter size={25} />
-					</Div>
-					<Div mx10 textSecondary2 clx={"group transition hover:text-primary-light"} cursorPointer aTag href={"https://instagram.com/weirdbears"}>
-						<FaInstagram size={25} />
-					</Div>
-					{/* <Div
+            </Div>
+          </Div>
+        )}
+        <BetterWorldModal />
+      </>
+    );
+  }
+  return (
+    <Div sticky={!absolute} absolute={absolute} top0 wFull z200 px80 py12 bgSecondary={!absolute} bgBlack={absolute} bgOpacity30={absolute}>
+      <MintingModal />
+      <InviteModal />
+      <RebirthModal />
+      <BetterWorldModal />
+      <AlertModal />
+      <Div flex mxAuto maxW={1150}>
+        <Div onClick={handleClickWebe} cursorPointer>
+          <Div w100 imgTag src={IMAGES.logos.webeLogoProduction}></Div>
+        </Div>
+        <Div flex1></Div>
+        <Div flex flexRow itemsCenter>
+          {balance > 0 && (
+            <Div mr10 textSecondary2 cursorPointer onClick={handleClickCheckOnMyWebes}>
+              <Div roundedFull py4 pr10 fontSize12 trackingWidest clx={"group transition hover:text-primary-light"}>
+                MY WEBES
+              </Div>
+            </Div>
+          )}
+          <Div mr10 textSecondary2 clx={"group transition hover:text-primary-light"} cursorPointer aTag href={"https://discord.com/invite/7tV3WxWf8p"}>
+            <FaDiscord size={30} />
+          </Div>
+          <Div mx10 clx={"group transition hover:opacity-50"} cursorPointer aTag href={"https://www.betterworldapp.io"}>
+            <Div imgTag src={IMAGES.logos.betterWorld} h25 wAuto></Div>
+          </Div>
+          <Div mx10 textSecondary2 clx={"group transition hover:text-primary-light"} cursorPointer aTag href={"https://twitter.com/webe_weirdbears"}>
+            <FaTwitter size={25} />
+          </Div>
+          <Div mx10 textSecondary2 clx={"group transition hover:text-primary-light"} cursorPointer aTag href={"https://instagram.com/weirdbears"}>
+            <FaInstagram size={25} />
+          </Div>
+          {/* <Div
 						mx10
 						textSm
 						textPrimary={locale == LOCALES.KO}
@@ -227,10 +194,10 @@ function MainTopBar({ absolute = false }) {
 					>
 						ENG
 					</Div> */}
-				</Div>
-			</Div>
-		</Div>
-	);
+        </Div>
+      </Div>
+    </Div>
+  );
 }
 
 export default MainTopBar;
