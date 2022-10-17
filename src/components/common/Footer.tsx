7cd450dfc91eaa33c66d20e08e818b166b86657f
jiyong-j <98527707+jiyong-j@users.noreply.github.com>
2022-10-17 16:07:25 +0900

    fix: Footer Logo

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -7,35 +7,35 @@ import Row from "../Row";
 import useIsTablet from "src/hooks/useIsTablet";
 
 export default function Footer() {
-	const handleClickGomz = () => {
-		href(urls.index);
-	};
-	const isTablet = useIsTablet();
+  const handleClickGomz = () => {
+    href(urls.index);
+  };
+  const isTablet = useIsTablet();
 
-	return (
-		<Div mxAuto maxW={1150} py30 px30={isTablet}>
-			<Row textWhite itemsCenter>
-				<Col auto onClick={handleClickGomz}>
-					<Div imgTag src={IMAGES.logos.webeLogo} h={isTablet ? 30 : 60}></Div>
-				</Col>
-				<Col py20>
-					<Row py5>
-						<Col auto>
-							<Div spanTag>WeBe from </Div>
-							<Div spanTag textSecondary2 aTag href={"https://soonilabs.com"}>
-								SOONI Labs
-							</Div>
-						</Col>
-						<Col></Col>
-					</Row>
-					<Row textSm>
-						<Col auto>
-							<Div spanTag>COPYRIGHT © WeBe. ALL RIGHTS RESERVED</Div>
-						</Col>
-						<Col></Col>
-					</Row>
-				</Col>
-			</Row>
-		</Div>
-	);
+  return (
+    <Div mxAuto maxW={1150} py30 px30={isTablet}>
+      <Row textWhite itemsCenter>
+        <Col auto onClick={handleClickGomz}>
+          <Div imgTag src={IMAGES.logos.webeLogoProduction} h={isTablet ? 50 : 60} style={{ marginTop: isTablet ? 0 : 10 }}></Div>
+        </Col>
+        <Col py20>
+          <Row py5>
+            <Col auto>
+              <Div spanTag>WeBe from </Div>
+              <Div spanTag textSecondary2 aTag href={"https://soonilabs.com"}>
+                SOONI Labs
+              </Div>
+            </Col>
+            <Col></Col>
+          </Row>
+          <Row textSm>
+            <Col auto>
+              <Div spanTag>COPYRIGHT © WeBe. ALL RIGHTS RESERVED</Div>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Col>
+      </Row>
+    </Div>
+  );
 }
